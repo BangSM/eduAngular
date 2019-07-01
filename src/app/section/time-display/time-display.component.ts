6dbd96f8ea22b2a02a544817b62d52389bd4e113
bangseungmin <bang3957@gmail.com>
2019-07-02 00:25:32 +0900

    component

---
+++ src/app/section/time-display/time-display.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-time-display',
+  templateUrl: './time-display.component.html',
+  styleUrls: ['./time-display.component.scss']
+})
+export class TimeDisplayComponent implements OnInit {
+
+  @Input() inputData: string;
+  test = 1;
+
+  constructor() {
+
+    console.log(this.inputData);
+    // setInterval(()=>{
+    //   this.test++
+    // }, 1000)
+  }
+
+  ngOnChanges(changes){
+
+  } 
+
+  ngOnInit() {
+
+
+  }
+
+}
